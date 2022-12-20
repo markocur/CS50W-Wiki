72ad4a216f97496a229d913b6619fb215ef5574a
markocur <mariusz.m.koch@gmail.com>
2022-12-20 20:29:01 +0100

    first-commit

---
+++ encyclopedia/views.py
@@ -0,0 +1,16 @@
+from django.shortcuts import render
+
+from . import util
+
+
+def index(request):
+    return render(request, "encyclopedia/index.html", {
+        "entries": util.list_entries()
+    })
+
+def entry(request, title):
+    return render(request, "encyclopedia/entry.html", {
+        "title": title,
+        "contents": util.get_entry(title)
+    })
+
