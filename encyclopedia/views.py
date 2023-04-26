867ea351b9c40a89053e18ef382c402ed7405646
Mariusz Koch <mariusz.koch@DIOTS.US.EDU.PL>
2023-04-26 15:23:03 +0200

    refactor: modified function for creating new pages to fulfill project requirements

--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 import markdown2
 from . import util
 from django import forms
@@ -6,8 +6,8 @@ from django.http import HttpResponseRedirect
 from django.urls import reverse
 
 class NewPageForm(forms.Form):
-    title = forms.CharField(label="New Page Title")
-    content = forms.CharField(widget=forms.Textarea, label="New Page Content")
+    title = forms.CharField(widget=forms.TextInput(attrs={'class':'form-control','placeholder':'Enter title of your page'}))
+    content = forms.CharField(widget=forms.Textarea(attrs={'class':'form-control','placeholder':'Enter contents of your page in Markdown'}))
 
 def index(request):
     return render(request, "encyclopedia/index.html", {
@@ -33,8 +33,15 @@ def new(request):
         if form.is_valid():
             title = form.cleaned_data["title"]
             content = form.cleaned_data["content"]
-            util.save_entry(title, content)
-            return HttpResponseRedirect(reverse("new"))
+            if util.get_entry(title) == None:
+                util.save_entry(title, content)
+                return redirect(entry, title=title)
+            else:
+                return render(request, "encyclopedia/error2.html", {
+                    "title": title,
+                })
+        
+            # add message about succesfully saving new entry 
         else:
             return render(request, "encyclopedia/new.html", {
                 "form": form
